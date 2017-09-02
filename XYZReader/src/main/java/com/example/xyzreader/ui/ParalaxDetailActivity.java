dd92d50aba062d3b1b11cf3e6d55cfd540f51f8e
Tiago Barbosa <tfreirebar@gmail.com>
2017-09-01 23:19:05 -0300

    feat: cardview colors

--- XYZReader/src/main/java/com/example/xyzreader/ui/ParalaxDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ParalaxDetailActivity.java
@@ -1,23 +1,20 @@
 package com.example.xyzreader.ui;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.app.LoaderManager;
-import android.content.Loader;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.xyzreader.R;
-import com.example.xyzreader.data.ArticleLoader;
-import com.example.xyzreader.data.ItemsContract;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -34,7 +31,7 @@ public class ParalaxDetailActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_detail_paralax);
 
-        myArticlesUtils ma = new myArticlesUtils(getIntent().getExtras());
+        final myArticlesUtils ma = new myArticlesUtils(getIntent().getExtras());
         mCursor = ma.loadContent();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -54,6 +51,19 @@ public class ParalaxDetailActivity extends AppCompatActivity{
 
         ImageView imageView = (ImageView) findViewById(R.id.header);
         Picasso.with(this).load(mCursor.getString(ma.getImage())).into(imageView);
+
+        FloatingActionButton myFabShare = (FloatingActionButton) findViewById(R.id.my_fab_share);
+        myFabShare.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String shareText = mCursor.getString(ma.getTitle()) + " by " + mCursor.getString(ma.getAuthor());
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,shareText);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
     }
 
 
@@ -61,6 +71,7 @@ public class ParalaxDetailActivity extends AppCompatActivity{
 
         private Bundle bundle;
         private int title = 2;
+        private int author = 3;
         private int body = 4;
         private int image = 6;
 
@@ -86,6 +97,10 @@ public class ParalaxDetailActivity extends AppCompatActivity{
 
         public int getImage(){
             return image;
+        }
+
+        public int getAuthor(){
+            return author;
         }
     }
 }
